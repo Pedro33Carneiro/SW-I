59d50c2d5147b9c17f9321941138158f2153a0d5
Pedro33Carneiro <124801768+Pedro33Carneiro@users.noreply.github.com>
2024-03-15 12:44:09 -0300

    Add files via upload

---
+++ Projetos/ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    public class Conta
+    {
+        private string? Num_conta { get; set; }
+        private double Saldo { get; set; }
+        private double limite { get; set; }
+     
+        public void consulta_saldo()
+        {
+            Console.WriteLine("o seu saldo atual é de R$ " + Saldo);
+        }
+        public void depositar(double valor)
+        {
+            Saldo += valor;
+        }
+        public void sacar(double valor)
+        {
+            if (valor <= Saldo )
+            {
+                Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("SALDO INSUFICIENTE DOIDÃO!!!!!")
+            }
+      
+        }
+        public void ajuste_limite(double valor)
+        {
+            limite = valor;
+        }
+    }
+}
---
+++ Projetos/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao banco ETEC MCM!");
+
+            //instalar a classe Conta e criar um objeto dela
+            Conta conta = new Conta();
+            Console.WriteLine(CONSULTANDO O SALDO);
+            conta.consulta_saldo()
+            Console.WriteLine(DEPOSITO DE 1000.35);
+            conta.depositar(1000.35)
+            Console.WriteLine(CONSULTANDO O SALDO);
+            conta.consulta_saldo()
+
+            //Console.WriteLine(SAQUE DE 500);
+            //conta.sacar(500)
+            //Console.WriteLine(CONSULTANDO O SALDO);
+            //conta.consulta_saldo()
+
+           
+
+            
+        }
+    }
+}
