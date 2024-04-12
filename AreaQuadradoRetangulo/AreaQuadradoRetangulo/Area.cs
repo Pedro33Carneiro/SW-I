718766eed461b8a470aae586c3a8c317b5fa91f8
Pedro33Carneiro <124801768+Pedro33Carneiro@users.noreply.github.com>
2024-04-12 12:33:58 -0300

    Add files via upload

---
+++ AreaQuadradoRetangulo/AreaQuadradoRetangulo/Area.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaQuadradoRetangulo
+{
+    public class Area
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+        public int AreaCalculada { get; set;}
+
+        public int Calcular()
+        {
+            AreaCalculada = Base * Altura;
+            return AreaCalculada;
+        }
+
+        public string Calcular2()
+        {
+            string Resultado = "";
+            AreaCalculada = Base * Altura;
+
+            if (Base == Altura) {
+                Resultado = "QUADRADO";
+            }
+            else
+            {
+                Resultado = "RETÂNGULO";
+            }
+            return Resultado;
+        }
+
+        public void MostraDetalhes()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("DETALHES");
+            Console.WriteLine("Base: " + Base);
+            Console.WriteLine("Altura: " + Altura);
+            Console.WriteLine("Área: " + AreaCalculada);
+        }
+
+    }
+}
---
+++ AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CÁLCULO DE ÁREA QUADRADO OU RETÂNGULO");
+            Area area= new Area();
+            Console.WriteLine("Digite o valor da Base");
+            area.Base = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o valor da Altura");
+            area.Altura = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("O valor da área é " + area.Calcular() + " e o formato é " + area.Calcular2());
+            Console.WriteLine("O valor da área é: " + area.Calcular());
+            Console.WriteLine("O formato é: " + area.Calcular2());
+
+            area.MostraDetalhes();
+
+
+        }
+    }
+}
